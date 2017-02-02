1e48160b467eae4e8be4010cd6300891c28e2dff
Kevin Cameron <kevinacameron@hotmail.com>
2017-02-02 12:24:09 -0800

    Working

--- test/index.js
+++ test/index.js
@@ -1,13 +1,18 @@
 "use strict";
 
 const spritzer = require("../lib"),
-    glob = "./test/fixtures/*.svg";
+    glob = "./test/fixtures/*.svg",
+    fsp  = require("fs-promise");
 
 spritzer(glob)
     .then((data) => {
         console.log(data);
+
+        return data;
     })
+    .then(fsp.writeFile.bind(fsp,"asdf.svg"))
     .catch((err) => {
         console.log("ERr");
         console.log(err);
+
     });
