f3aace6522a9a861acabcfc890d8dae4ba632aa9
kevinkace <kevinacameron@hotmail.com>
2017-02-03 13:30:50 -0800

    Mocha/Assert for test

--- test/index.js
+++ test/index.js
@@ -5,15 +5,18 @@ const spritzer = require("../lib"),
     fsp = require("fs-promise"),
 
     glob     = "./test/fixtures/input/*.svg",
-    expected = "./test/fixtures/output/expected.svg";
+    expected = "./test/fixtures/output/expected.svg",
 
-Promise.all([
-        spritzer(glob),
-        fsp.readFile(expected, "utf8")
-    ])
-    .then((data) => {
-        console.log(data[0] === data[1]);
-    })
-    .catch((err) => {
-        console.log(`Error: ${err}`);
+    assert = require("assert");
+
+describe("/lib/index.js", () => {
+    it("should process icons that match the expected file", () => {
+        Promise.all([
+            spritzer(glob),
+            fsp.readFile(expected, "utf8")
+        ])
+        .then((data) => {
+            assert.equal(data[0], data[1]);
+        });
     });
+});
